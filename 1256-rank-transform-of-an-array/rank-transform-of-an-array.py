7eb71d5c52dbef1185b5f7e238cb4f2029ff4ef0
Rajat Masanagi <143593276+rajat-masanagi@users.noreply.github.com>
2024-10-02 10:24:25 +0530

    Time: 237 ms (75.02%) | Memory: 35.9 MB (21.23%) - LeetSync

--- 1256-rank-transform-of-an-array/rank-transform-of-an-array.py
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.py
@@ -1,13 +1,11 @@
-import numpy as np
 class Solution:
     def arrayRankTransform(self, arr: List[int]) -> List[int]:
-        ina=np.argsort(arr)
-        ins=np.sort(arr)
+        ins=sorted(list(set(arr)))
+        d={}
+        for i in range(len(ins)):
+            d[ins[i]]=i+1
         ans=[1]*len(arr)
-        for i in range(1,len(arr)):
-            if ins[i-1]==ins[i]:
-                ans[ina[i]]=ans[ina[i-1]]
-            else:
-                ans[ina[i]]=ans[ina[i-1]]+1
+        for i in range(len(arr)):
+            ans[i]=d[arr[i]]
         return ans
             
