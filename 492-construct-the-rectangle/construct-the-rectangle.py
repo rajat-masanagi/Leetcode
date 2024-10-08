e8715984f10e8ff2fe410425ee9ced269c6a6b26
Rajat Masanagi <143593276+rajat-masanagi@users.noreply.github.com>
2024-10-08 17:42:07 +0530

    Time: 40 ms (43.06%) | Memory: 16.5 MB (48.77%) - LeetSync

--- 492-construct-the-rectangle/construct-the-rectangle.py
+++ 492-construct-the-rectangle/construct-the-rectangle.py
@@ -1,18 +1,5 @@
 class Solution:
     def constructRectangle(self, area: int) -> List[int]:
-        divs=[]
-        for i in range(1,area):
-            if area%i==0:
-                if i>area//i:          
-                    divs.append([i,area//i])
-                else:
-                    divs.append([area//i,i])
-        min_diff=area
-        min_index=0
-        for i in range(len(divs)):
-            if abs(divs[i][0]-divs[i][1])<min_diff:
-                min_diff=abs(divs[i][0]-divs[i][1])
-                min_index=i
-        if area==1:
-            return [1,1]
-        return divs[min_index]
+        for l in range(int(area**0.5), 0, -1):            
+            if area % l == 0: 
+                return [area // l, l]
