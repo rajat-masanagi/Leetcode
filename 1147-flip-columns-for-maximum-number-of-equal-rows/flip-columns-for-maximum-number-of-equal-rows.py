e4d4198b2fc0b3fbd24fad5510f0c01b452fc9d5
Rajat Masanagi <143593276+rajat-masanagi@users.noreply.github.com>
2024-11-22 21:47:03 +0530

    Time: 19 ms (100.00%) | Memory: 19.9 MB (28.64%) - LeetSync

--- 1147-flip-columns-for-maximum-number-of-equal-rows/flip-columns-for-maximum-number-of-equal-rows.py
+++ 1147-flip-columns-for-maximum-number-of-equal-rows/flip-columns-for-maximum-number-of-equal-rows.py
@@ -1,12 +1,20 @@
 class Solution:
     def maxEqualRowsAfterFlips(self, matrix: List[List[int]]) -> int:
         count = defaultdict(int)
-        
+
         for row in matrix:
             row_key = tuple(row)
 
             if row[0]:
-                row_key = tuple([0 if n else 1 for n in row])
+                # Check Flipped Tuples
+                new_list = []
+                for n in row:
+                    if n == 1:
+                        new_list.append(0)
+                    else:
+                        new_list.append(1)
+                row_key = tuple(new_list)
+
 
             count[row_key] += 1
 
