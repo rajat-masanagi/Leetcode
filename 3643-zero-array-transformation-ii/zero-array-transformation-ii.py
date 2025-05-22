1d66712c23f1af8716ed8ee7bc0efd6bbc7a9bfa
Rajat Masanagi <143593276+rajat-masanagi@users.noreply.github.com>
2025-05-22 19:57:49 +0530

    Time: 158 ms (72.91%) | Memory: 64.9 MB (7.61%) - LeetSync

--- 3643-zero-array-transformation-ii/zero-array-transformation-ii.py
+++ 3643-zero-array-transformation-ii/zero-array-transformation-ii.py
@@ -1,26 +1,44 @@
 class Solution:
     def minZeroArray(self, nums: List[int], queries: List[List[int]]) -> int:
-        def canzero(nums,queries):
-            arr=[0]*(len(nums)+1)
-            for i in range(len(queries)):
-                arr[queries[i][0]]-=queries[i][2]
-                arr[queries[i][1]+1]+=queries[i][2]
-            
-            sumi=0
-            for i in range(len(nums)):
-                sumi+=arr[i]
-                if nums[i]>-sumi:
-                    return False
-            return True
-        
-        left=0
-        right=len(queries)
-        ans=-1
-        while left<=right:
-            mid=(right+left)//2
-            if canzero(nums,queries[:mid])==True:
-                ans=mid
-                right=mid-1
-            else:
-                left=mid+1
-        return ans
+        # def canzero(nums,queries):
+        #     arr=[0]*(len(nums)+1)
+        #     for i in range(len(queries)):
+        #         arr[queries[i][0]]-=queries[i][2]
+        #         arr[queries[i][1]+1]+=queries[i][2]
+        #     sumi=0
+        #     for i in range(len(nums)):
+        #         sumi+=arr[i]
+        #         if nums[i]>-sumi:
+        #             return False
+        #     return True
+
+        # left=0
+        # right=len(queries)
+        # ans=-1
+        # while left<=right:
+        #     mid=(right+left)//2
+        #     if canzero(nums,queries[:mid])==True:
+        #         ans=mid
+        #         right=mid-1
+        #     else:
+        #         left=mid+1
+        # return ans
+
+        #Linesweep
+        arr=[0]*(len(nums)+1)
+        k=0
+        total=0
+        for i in range(len(nums)):
+            while total<nums[i]-arr[i]:
+                if k>=len(queries):
+                    return -1
+                l,r,v=queries[k]
+                if r<i: # if i already past right idx of current query skip
+                    k+=1
+                    continue
+                arr[max(l,i)]+=v # skip previous i
+                arr[r+1]-=v
+                k+=1
+            total+=arr[i]
+
+        return k
